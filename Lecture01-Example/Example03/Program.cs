92fca826434f57694e27c456e51705a1aabdc813
hungei0601 <hungwei0601@gmail.com>
2017-05-29 19:42:45 +0800

    第一講範例更新

--- Lecture01-Example/Example03/Program.cs
+++ Lecture01-Example/Example03/Program.cs
@@ -13,6 +13,14 @@ namespace Example03
             Student someone = new Student();
             someone.name = "盧弘偉";
             Console.WriteLine("學生姓名:{0}", someone.name);
+            someone.birthday = "1979年6月1日";
+            Console.WriteLine("生日:{0}", someone.birthday);
+            someone.email = "hungwei0601@gmail.com";
+            Console.WriteLine("電子信箱:{0}", someone.email);
+            someone.phone = "0912345678";
+            Console.WriteLine("連絡電話:{0}", someone.phone);
+            someone.phone = "38";
+            Console.WriteLine("年齡:{0}", someone.phone);
         }
     }
 }
